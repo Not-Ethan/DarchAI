782a013b0ffbad3b2e91477232319108bd54f11a
Ethan W <ethan.wanq@gmail.com>
2023-04-10 00:54:11 -0400

    create back end async request processing

--- app.py
+++ app.py
@@ -1,37 +1,42 @@
 from flask import Flask, request, jsonify
 import main
-import pdb
-from pprint import pprint
+import uuid
+import threading
 
 app = Flask(__name__)
 
-# Define an endpoint for processing the user's input
+requests_data = {}
+
 @app.route('/process_input', methods=['POST'])
 def process_input():
-    print("Recieved request")
-    # Get the input data from the request's JSON payload
-    data = request.get_json()
-    topic = data['topic']
-    side = data['side']
-    argument = data['argument']
-    num = data['num'] if 'num' in data else 10
+    topic = request.form.get('topic')
+    side = request.form.get('side')
+    argument = request.form.get('argument')
+    num = int(request.form.get('num'))
 
-    # Call your AI function with the provided data
-    result = main.main(topic, side, argument, num)
+    request_id = str(uuid.uuid4())
+    requests_data[request_id] = None  # Initialize the request data to None
+
+    def process_request():
+        result = main.main(topic, side, argument, num, request_id=request_id)
+        requests_data[request_id] = result
+
+    # Process the request in a separate thread
+    threading.Thread(target=process_request).start()
     
-    # Return the result as a JSON response
-    return jsonify(result)
+    return jsonify({'status': 'success', 'request_id': request_id})
+
+@app.route('/check_progress', methods=['GET'])
+def check_progress():
+    request_id = request.args.get('request_id')
+    if request_id not in requests_data:
+        return jsonify({'status': 'error', 'message': 'Invalid request ID'})
+
+    if request_id in main.progress:
+        progress_value = main.progress[request_id]
+        return jsonify({'status': 'processing', 'progress': progress_value})
+    else:
+        return jsonify({'status': 'completed', 'result': requests_data[request_id]})
 
 if __name__ == '__main__':
-    # Run the Flask app on localhost and an internal port
-    app.run(host='127.0.0.1', port=5000)
-
-'''
-Should not ban the collection of personal data through biometric recognition technology
-    arguments = [
-        {
-            "side": "sup",
-            "argument": "utilitarianism is bad"
-        }
-    ]    
-'''
+    app.run(debug=True)
