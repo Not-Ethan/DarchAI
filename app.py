82224d6296ebebc5bf8931369ad3eea57677cda6
Ethan W <ethan.wanq@gmail.com>
2023-04-10 19:42:35 -0400

    fix formatting and remove print statements

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ def process_input():
     topic = request.json.get('topic')
     side = request.json.get('side')
     argument = request.json.get('argument')
-    print(topic, side, argument)
     num = int(request.json.get('num')) if request.json.get('num') else 10
 
     request_id = str(uuid.uuid4())
@@ -22,10 +21,11 @@ def process_input():
     def process_request():
         try:
             result = main.main(topic, side, argument, num, request_id=request_id)
-            requests_data[request_id] = {'status': 'success', 'result': result}
+            requests_data[request_id] = {'status': 'complete', 'result': result}
         except Exception as e:
             error_message = str(e)
             requests_data[request_id] = {'status': 'error', 'message': error_message}
+        
 
     # Process the request in a separate thread
     threading.Thread(target=process_request).start()
@@ -41,9 +41,9 @@ def check_progress():
 
     if request_id in main.progress:
         progress_value = main.progress[request_id]
-        return jsonify({'status': 'processing', 'progress': progress_value})
+        return jsonify({'status': 'processing', 'progress': progress_value, 'id': request_id})
     else:
-        return jsonify({'status': 'completed', 'result': requests_data[request_id]})
+        return jsonify({'status': 'completed', 'result': requests_data[request_id], 'id': request_id})
 
 if __name__ == '__main__':
     app.run(debug=True)
