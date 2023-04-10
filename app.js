e5f332538243ce72fcaa888d2a14754413a13038
Ethan W <ethan.wanq@gmail.com>
2023-04-10 00:53:45 -0400

    rename main.js to app.js (to avoid repeat name with front end js)

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require("express")
 const app = express()
 
 app.use(express.static("public"))
+app.set('view engine', 'ejs')
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname+"/views/index.html");
+    res.render(__dirname+"/views/index.html");
 })
 
 app.listen(3000, '0.0.0.0', () => {
