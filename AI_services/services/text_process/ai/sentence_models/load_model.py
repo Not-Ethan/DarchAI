051ac7399413f6a825e921c81b449a817f79b7ec
Ethan W <ethan.wanq@gmail.com>
2023-05-29 12:47:07 -0400

    fix typing

--- AI_services/services/text_process/ai/sentence_models/load_model.py
+++ AI_services/services/text_process/ai/sentence_models/load_model.py
@@ -3,7 +3,7 @@ from services.text_process.ai.sentence_models.model_loader.roberta_base import r
 from services.text_process.ai.sentence_models.model_loader.deberta_base import deberta_base
 from services.text_process.ai.sentence_models.model_loader.distilbert import distilbert
 
-def load_model(sentence1: str, sentence2: str, sentence_model=0) -> tuple[float, float]:
+def load_model(sentence1: str, sentence2: str, sentence_model=0):
     if sentence_model == 0:
         return (distilbert(sentence1, sentence2), 0.75)
     if sentence_model == 1:
