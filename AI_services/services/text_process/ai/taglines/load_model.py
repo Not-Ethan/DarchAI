90b215cdec4e3018f34b426aca1ce30f4eaa6dfc
Ethan W <ethan.wanq@gmail.com>
2023-05-29 11:45:53 -0400

    tagline stuff without the models

---
+++ AI_services/services/text_process/ai/taglines/load_model.py
@@ -0,0 +1,9 @@
+from services.text_process.ai.taglines.model_loader.bart_base import generate_tagline as bart_base
+from services.text_process.ai.taglines.model_loader.t5_small import generate_tagline as t5_small
+def load_model(text, model=0):
+    if model == 0:
+        return bart_base(text)
+    elif model == 1:
+        return t5_small(text)
+    else:
+        raise Exception("Tagline model not found")
