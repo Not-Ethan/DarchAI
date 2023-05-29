ad9f64f684a01155333945542a87fa26e17b8e03
Ethan W <ethan.wanq@gmail.com>
2023-05-29 11:33:17 -0400

    refactor backend structure

--- public/js/register.js
+++
@@ -1,5 +0,0 @@
-function onSignIn(googleUser) {
-  // Get the user's ID token, which you can send to your server
-  const id_token = googleUser.getAuthResponse().id_token;
-  console.log("ID Token: " + id_token);
-}
